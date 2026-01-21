b2c8605dca5b90224048f3c1b28d73aba6b476e0
Loren Otermin <99lotermin@gmail.com>
2026-01-21 23:51:41 +0100

    asi funcionaria pagina a pagina

--- cli.py
+++ cli.py
@@ -1,42 +1,78 @@
 import pandas as pd
 from docx import Document
+from docxtpl import DocxTemplate
+from datetime import date
 
-def reemplazar_texto(doc, reemplazos: dict):
-    for p in doc.paragraphs:
-        for clave, valor in reemplazos.items():
-            if clave in p.text:
-                p.text = p.text.replace(clave, str(valor))
+def funcionConDocxtpl(df):
+    # pasar a lista de dicts
+    socios = df.to_dict(orient="records")
 
-def copiar_plantilla(doc_destino, doc_origen):
-    for p in doc_origen.paragraphs:
-        nuevo_p = doc_destino.add_paragraph(p.text)
-        nuevo_p.style = p.style
+    # cargar plantilla
+    doc = DocxTemplate("files/Ziurtagiria.docx")
+
+    # renderizar
+    doc.render({
+        "socios": socios
+    })
+
+    # guardar resultado
+    doc.save("Ziurtagiriak_MECNA.docx")
+
+
+def funcionConDocx(lista_socios):
+    doc = Document('files/Ziurtagiria2.docx')
+
+    for idx, socio in enumerate(lista_socios):
+        # list of all words to be replaced, with its new word
+        replace_word = {'{{Izena}}':'Loren', '{{Abizenak}}':'Otermin Motos','{{NAN_zkia}}':'4564654','{{Zenbatekoa}}':'90'}
+
+        # to replace words within paragraphs
+        for word in replace_word:
+            for p in doc.paragraphs:
+                if p.text.find(word) >= 0:
+                    p.text = p.text.replace(word, replace_word[word])
+
+        # to replace words within tables
+        for word in replace_word:
+            for table in doc.tables:
+                for row in table.rows:
+                    for cell in row.cells:
+                        for p in cell.paragraphs:
+                            if p.text.find(word) >= 0:
+                                p.text = p.text.replace(word, replace_word[word])
+
+        # to replace words within headers
+        for word in replace_word:
+            for section in doc.sections:
+                header = section.header
+                for table in doc.tables:
+                    for row in table.rows:
+                        for cell in row.cells:
+                            for p in cell.paragraphs:
+                                if p.text.find(word) >= 0:
+                                    p.text = p.text.replace(word, replace_word[word])
+
+        # to replace words within footers
+        for word in replace_word:
+            for footer in doc.sections:
+                footer = section.footer
+                for table in doc.tables:
+                    for row in table.rows:
+                        for cell in row.cells:
+                            for p in cell.paragraphs:
+                                if p.text.find(word) >= 0:
+                                    p.text = p.text.replace(word, replace_word[word])
+    doc.save('Output.docx')
 
 
 if __name__ == '__main__':
     mecna_names = pd.read_excel("files/MECNA.xlsx", usecols="A:D", header=0, index_col=None,
                                 sheet_name="2025 (uztailetik)")
 
-    plantilla = Document("files/Ziurtagiria.docx")
-    doc_final = Document()
+    mecna_names = mecna_names.rename(columns={"NAN zkia": "NAN_zkia"})
+    mecna_names = mecna_names.iloc[:2,:]
 
-    for i, socio in mecna_names.iterrows():
-        doc_temp = plantilla
+    # funcionConDocxtpl(mecna_names)
+    funcionConDocx(mecna_names)
 
-        reemplazos = {
-            "{{Izena}}": socio["Izena"],
-            "{{Abizenak}}": socio["Abizenak"],
-            "{{NAN_zkia}}": socio["NAN zkia"],
-            "{{Zenbatekoa}}": socio["Zenbatekoa"],
-        }
-
-        reemplazar_texto(doc_temp, reemplazos)
-
-        copiar_plantilla(doc_final, doc_temp)
-
-        if i == 2:
-            break
-        if i != len(mecna_names) - 1:
-            doc_final.add_page_break()
-
-    doc_final.save("files/documento_mecna.docx")
+    exit()
