7730b828954a351a7efadf42baf844533c0d43ec
Loren Otermin <99lotermin@gmail.com>
2026-01-22 18:57:32 +0100

    v1.1 conversion to pdf

--- cli.py
+++ cli.py
@@ -1,19 +1,20 @@
 import pandas as pd
-from docx import Document
+from docx2pdf import convert
 from docxtpl import DocxTemplate
-from datetime import date, datetime
+from datetime import datetime
+import docx2pdf
 
 
 def funcionConDocxtpl(df):
-    # pasar a lista de dicts
-    print(df["Izena"])
+
+    # Pasar a dict para poder reccorer
     socios = df.to_dict(orient="records")
 
-    # cargar plantilla
+    # Cargar plantilla
     doc = DocxTemplate("files/Ziurtagiria1.docx")
     # doc = DocxTemplate("files/Prueba1.docx")
 
-    # renderizar
+    # Renderizar
     doc.render({
         "socios": socios,
         "salto": "\n\n\n\n\n\n\n\n",
@@ -23,7 +24,7 @@ def funcionConDocxtpl(df):
     # guardar resultado
     doc.save("files/Ziurtagiriak_MECNA.docx")
 
-
+    return doc
 
 if __name__ == '__main__':
     mecna_names = pd.read_excel("files/MECNA.xlsx", usecols="A:D", header=0, index_col=None,
@@ -31,6 +32,8 @@ if __name__ == '__main__':
 
     mecna_names = mecna_names.rename(columns={"NAN zkia": "NAN_zkia"})
 
-    funcionConDocxtpl(mecna_names)
+    doc = funcionConDocxtpl(mecna_names)
+    # Convertir a pdf
+    convert("files/Ziurtagiriak_MECNA.docx", "files/Ziurtagiria_MECNA.pdf")
 
     exit()
