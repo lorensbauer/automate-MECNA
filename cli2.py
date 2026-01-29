7fd8800d90fab9debe2d10706ec6b349051e7c1d
Loren Otermin <99lotermin@gmail.com>
2026-01-29 23:07:48 +0100

    v3.0.0 versión más rapida crear todo el pdf a la vez

---
+++ cli2.py
@@ -0,0 +1,62 @@
+from tempfile import TemporaryDirectory
+
+from PyPDF2 import PdfReader, PdfWriter
+import pandas as pd
+from docx2pdf import convert
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+from core.certificatePDF import sign_pdf
+from core.fillTemplate import funcionConDocxtpl
+from core.sendEmail import send_email
+
+load_dotenv()
+if __name__ == "__main__":
+    plantilla_path = "input/Ziurtagiria2.docx"
+    mecna_names = pd.read_excel(
+        io="input/MECNA _korreoekin.xlsx",
+        usecols="A:E",
+        header=0,
+        index_col=None,
+        sheet_name="2025 (uztailetik)",
+    )
+    mecna_names = mecna_names.rename(columns={"NAN zkia": "NAN_zkia"})
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        # 1. Rellenar plantilla usando docxtpl
+        doc = funcionConDocxtpl(mecna_names)
+        docx_path = tmpdir / "plantillaRellenada.docx"
+        doc.save(docx_path)
+
+        # 2. Convertir a pdf
+        pdf_path = tmpdir / "doc2pdf.pdf"
+        convert(docx_path, pdf_path)
+
+        # 3. Dividir pdf en tantos pdfs como paginas
+
+        reader = PdfReader(pdf_path)
+
+        for i, page in enumerate(reader.pages):
+            if i != len(reader.pages) - 1:
+                writer = PdfWriter()
+                writer.add_page(page)
+                with open(tmpdir / "doc.pdf", "wb") as out:
+                    writer.write(out)
+                # Firmar documento
+                pdf = sign_pdf(tmpdir / "doc.pdf")
+                # Mandar por correo
+                # 4. Enviar por correo
+                subject = "MECNA ziurtagiria"
+                sender = "99lotermin@gmail.com"
+                recipients = [mecna_names.loc[i, "Email"]]
+                recipients_name = f'{mecna_names.loc[i, "Izena"]} {mecna_names.loc[i, "Abizenak"]}'
+                body = (
+                    f"Kaixo {recipients_name}\n"
+                    "Hemen duzue MECNA-ren ziurtagiria sinatuta. "
+                    "Mezu hau automatikoki sortu da, arazorik egotekotan idatzi hurrengo posta el"
+                )
+                password = os.getenv("PASSWORD")
+
+                send_email(subject, body, sender, recipients, password, pdf, recipients_name)
+
--- core/fillTemplate.py
+++ core/fillTemplate.py
@@ -8,9 +8,13 @@ import docx2pdf
 
 def fillTemplate(socio, plantilla_path):
     doc = Document(plantilla_path)
-    replace_word = {'{{Izena}}': str(socio["Izena"]), '{{Abizenak}}': str(socio["Abizenak"]),
-                    '{{NAN_zkia}}': str(socio["NAN_zkia"]), '{{Zenbatekoa}}': str(socio["Zenbatekoa"]),
-                    '{{data}}': datetime.now().strftime("%Y-%m-%d")}
+    replace_word = {
+        "{{Izena}}": str(socio["Izena"]),
+        "{{Abizenak}}": str(socio["Abizenak"]),
+        "{{NAN_zkia}}": str(socio["NAN_zkia"]),
+        "{{Zenbatekoa}}": str(socio["Zenbatekoa"]),
+        "{{data}}": datetime.now().strftime("%Y-%m-%d"),
+    }
     # to replace words within paragraphs
     for word in replace_word:
         for p in doc.paragraphs:
@@ -51,13 +55,17 @@ def fillTemplate(socio, plantilla_path):
 
 
 def funcionConDocx(lista_socios, replace_word):
-    doc = Document('../input/Ziurtagiria2.docx')
+    doc = Document("../input/Ziurtagiria2.docx")
 
     lista_socios = lista_socios.to_dict(orient="records")
     for idx, socio in enumerate(lista_socios):
         # list of all words to be replaced, with its new word
-        replace_word = {'{{Izena}}': str(socio["Izena"]), '{{Abizenak}}': str(socio["Abizenak"]),
-                        '{{NAN_zkia}}': str(socio["NAN_zkia"]), '{{Zenbatekoa}}': str(socio["Zenbatekoa"])}
+        replace_word = {
+            "{{Izena}}": str(socio["Izena"]),
+            "{{Abizenak}}": str(socio["Abizenak"]),
+            "{{NAN_zkia}}": str(socio["NAN_zkia"]),
+            "{{Zenbatekoa}}": str(socio["Zenbatekoa"]),
+        }
 
         # to replace words within paragraphs
         for word in replace_word:
@@ -103,15 +111,11 @@ def funcionConDocxtpl(df):
     socios = df.to_dict(orient="records")
 
     # Cargar plantilla
-    doc = DocxTemplate("../input/Ziurtagiria1.docx")
+    doc = DocxTemplate("./input/Ziurtagiria1.docx")
     # doc = DocxTemplate("input/Prueba1.docx")
 
     # Renderizar
-    doc.render({
-        "socios": socios,
-        "salto": "\n\n\n\n\n\n\n\n",
-        "data": datetime.now().strftime("%Y-%m-%d")
-    })
+    doc.render({"socios": socios, "salto": "\n\n\n\n\n\n\n\n", "data": datetime.now().strftime("%Y-%m-%d")})
 
     # guardar resultado
     doc.save("output/Ziurtagiriak_MECNA.docx")
@@ -119,9 +123,10 @@ def funcionConDocxtpl(df):
     return doc
 
 
-if __name__ == '__main__':
-    mecna_names = pd.read_excel("../input/MECNA.xlsx", usecols="A:D", header=0, index_col=None,
-                                sheet_name="2025 (uztailetik)")
+if __name__ == "__main__":
+    mecna_names = pd.read_excel(
+        "../input/MECNA.xlsx", usecols="A:D", header=0, index_col=None, sheet_name="2025 (uztailetik)"
+    )
 
     mecna_names = mecna_names.rename(columns={"NAN zkia": "NAN_zkia"})
     mecna_names = mecna_names.loc[:2, :]
