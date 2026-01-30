cf779ef4b3642f15c9ca97ba4f3f2f3480dd3c9a
Loren Otermin <99lotermin@gmail.com>
2026-01-30 23:02:32 +0100

    v3.2.0 opcion de hacer firma visible

--- core/certificatePDF.py
+++ core/certificatePDF.py
@@ -1,16 +1,19 @@
+import datetime
 import os
 from io import BytesIO
 
-from pyhanko.sign import signers
+from pyhanko.sign import signers, PdfSigner, fields
 from pyhanko.pdf_utils.incremental_writer import IncrementalPdfFileWriter
 from dotenv import load_dotenv
+from pyhanko.sign.fields import SigFieldSpec
+from pyhanko.stamp import TextStampStyle
 
 load_dotenv()
 
+
 def sign_pdf(path_pdf_to_sign):
     signer = signers.SimpleSigner.load_pkcs12(
-        'certIT.p12',
-        passphrase=os.getenv("PASSWORD_DEL_CERTIFICADO").encode()
+        "certIT.p12", passphrase=os.getenv("PASSWORD_DEL_CERTIFICADO").encode()
     )
 
     with open(path_pdf_to_sign, "rb") as pdf:
@@ -19,21 +22,58 @@ def sign_pdf(path_pdf_to_sign):
         signers.sign_pdf(
             writer,
             signature_meta=signers.PdfSignatureMetadata(
-                field_name="Firma_FNMT",
-                reason="Certificacion MECNA",
-                location="Pamplona"
+                field_name="Firma_FNMT", reason="Certificacion MECNA", location="Pamplona"
             ),
             signer=signer,
-            output=outputBuffer
+            output=outputBuffer,
         )
     outputBuffer.seek(0)
     return outputBuffer.getvalue()
 
 
+def sign_pdf_visible(path_pdf_to_sign):
+    # Cargar certificado FNMT
+    signer = signers.SimpleSigner.load_pkcs12(
+        "certIT.p12", passphrase=os.getenv("PASSWORD_DEL_CERTIFICADO").encode()
+    )
+
+    with open(path_pdf_to_sign, "rb") as f:
+        writer = IncrementalPdfFileWriter(f, strict=False)
+
+        # (Opcional pero recomendado) definir posición de la firma
+        fields.append_signature_field(
+            writer,
+            fields.SigFieldSpec(
+                sig_field_name="Firma_FNMT",
+                box=(50, 50, 300, 150),  # coordenadas
+                on_page=0,  # primera página
+            ),
+        )
+
+        meta = signers.PdfSignatureMetadata(
+            field_name="Firma_FNMT", reason="Certificación MECNA", location="Pamplona"
+        )
+
+        stamp = TextStampStyle(
+            stamp_text=(
+                "Iruña taldeko lehendakariak digitalki sinatutako dokumentua\n"
+                f"Data: {datetime.now().strftime("%Y-%m-%d")}\n"
+                "MECNA Zurtagiria"
+            )
+        )
+
+        pdf_signer = PdfSigner(signature_meta=meta, signer=signer, stamp_style=stamp)
+
+        output = BytesIO()
+        pdf_signer.sign_pdf(writer, output=output)
+
+    output.seek(0)
+    return output.getvalue()
+
+
 def firmar_pdf():
     signer = signers.SimpleSigner.load_pkcs12(
-        "../certIT.p12",
-        passphrase=os.getenv("PASSWORD_DEL_CERTIFICADO").encode()
+        "../certIT.p12", passphrase=os.getenv("PASSWORD_DEL_CERTIFICADO").encode()
     )
 
     # Firmar PDF
@@ -44,14 +84,13 @@ def firmar_pdf():
             signers.sign_pdf(
                 writer,
                 signature_meta=signers.PdfSignatureMetadata(
-                    field_name="Firma_FNMT",
-                    reason="Certificación MECNA",
-                    location="Pamplona"
+                    field_name="Firma_FNMT", reason="Certificación MECNA", location="Pamplona"
                 ),
                 signer=signer,
-                output=outf
+                output=outf,
             )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     firmar_pdf()
     print(os.getenv("PASSWORD_DEL_CERTIFICADO"))
